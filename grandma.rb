88851027c69c0ff57dc70c3307d3ae4ecdd0a861
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-21 18:29:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ if speak == speak.upcase
    return "NO, NOT SINCE 1938!"
 elsif speak == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
-elsif speak == "grandma"
+elsif speak == spead.downcase
    return "HUH?! SPEAK UP, SONNY!"
 end
 end
