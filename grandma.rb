0350e5412b88537302cf075e6b8fe920501d51db
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-21 18:28:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@
 def speak_to_grandma(speak)
 
 if speak == speak.upcase
-  puts "NO, NOT SINCE 1938!"
+   "NO, NOT SINCE 1938!"
 elsif speak == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
+   "I LOVE YOU TOO PUMPKIN!"
 elsif speak == "grandma"
-  puts "HUH?! SPEAK UP, SONNY!"
+   "HUH?! SPEAK UP, SONNY!"
 end
 end
